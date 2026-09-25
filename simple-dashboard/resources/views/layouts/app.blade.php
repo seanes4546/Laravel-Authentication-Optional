2514152107ab5feaa13242b9f65138af69ea5127
seanes4546 <sean.audhie@gmail.com>
2026-09-25 13:08:16 +0700

    New Files

--- simple-dashboard/resources/views/layouts/app.blade.php
+++ simple-dashboard/resources/views/layouts/app.blade.php
@@ -11,9 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('js/jquery.js') }}"></script>
+        <link href="https://jsdelivr.net" rel="stylesheet">
+        
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
--- simple-dashboard/resources/views/layouts/guest.blade.php
+++ simple-dashboard/resources/views/layouts/guest.blade.php
@@ -11,9 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('js/jquery.js') }}"></script>
+        <link href="https://jsdelivr.net" rel="stylesheet">
+        
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
